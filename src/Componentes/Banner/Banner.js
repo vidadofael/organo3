44bad2702f17ff2934a388205d9b87bfca0ac8f9
vidadofael <vidadofael@gmail.com>
2023-05-05 10:24:50 -0300

    iniciando aulas do projeto organo 3

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner Organo 3" />
+        </header>
+    )
+}
+export default Banner

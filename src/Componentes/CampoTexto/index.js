14c85350335fc44964da96a3f155117a90a52c9e
vidadofael <vidadofael@gmail.com>
2023-05-08 10:46:04 -0300

    finalizando validação de preenchimento de campos

--- src/Componentes/CampoTexto/index.js
+++ src/Componentes/CampoTexto/index.js
@@ -5,7 +5,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder} />
+            <input required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
